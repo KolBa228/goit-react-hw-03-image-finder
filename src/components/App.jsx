e8aaa92b221b9a328713a93d5b5a7dcb8c76e43f
KolBa228 <technotrashinfo@gmail.com>
2023-08-06 18:20:19 +0100

    done
    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,9 @@
 import React, { Component } from "react";
-// import axios from "axios";
 import { Searchbar } from "./Searchbar/Searchbar";
 import { ImageGallery } from "./ImageGallery/ImageGallery";
-// import { ImageGalleryItem } from "./ImageGalleryItem/ImageGalleryItem";
 import { Button } from "./Button/Button";
 import { RotatingLines } from "react-loader-spinner";
-import { fetchData } from "./Fetch/Fetch";
+import { fetchData } from "../fetch/fetch";
 import { Modal } from "./Modal/Modal";
 
 export class App extends Component {
@@ -20,18 +18,25 @@ export class App extends Component {
   };
 
   handleSearch = (query) => {
-  this.setState({ q: query, page: 1, images: []}, () => {
-    this.fetchData();
-  });
-};
+    this.setState({ q: query, page: 1, images: [] }, () => {
+      // this.fetchData();
+    });
+  };
 
+  handleKeyDown = (event) => {
+    if (event.keyCode === 27) {
+      this.closeModal();
+    }
+  };
 
   openModal = (imageUrl) => {
     this.setState({ selectedImage: imageUrl });
+    window.addEventListener("keydown", this.handleKeyDown);
   };
 
   closeModal = () => {
     this.setState({ selectedImage: null });
+    window.removeEventListener("keydown", this.handleKeyDown);
   };
 
   fetchData = async () => {
@@ -50,12 +55,18 @@ export class App extends Component {
       });
     }
   };
-  
+
+  // componentDidUpdate(prevProps, prevState) {
+  //   if (prevState.page !== this.state.page) {
+  //     this.fetchData();
+  //   }
+  // }
+
   componentDidUpdate(prevProps, prevState) {
-    if (prevState.page !== this.state.page) {
-      this.fetchData();
-    }
+  if (prevState.page !== this.state.page || prevState.q !== this.state.q) {
+    this.fetchData();
   }
+}
 
   onloadMore = () => {
     this.setState((prevState) => ({
@@ -69,23 +80,21 @@ export class App extends Component {
 
   render() {
     const { images, isLoading } = this.state;
-    // const isLoadMoreVisible = !isLoading && images.length >= 12 && images.length % this.state.per_page === 0;
 
     return (
       <>
         <Searchbar onSubmit={this.handleSearch} />
         {isLoading ? (
-  <RotatingLines
-    strokeColor="grey"
-    strokeWidth="5"
-    animationDuration="0.75"
-    width="96"
-    visible={true}
-  />
-) : (
-  <ImageGallery images={images} openModal={this.openModal} />
-)};
-
+          <RotatingLines
+            strokeColor="grey"
+            strokeWidth="5"
+            animationDuration="0.75"
+            width="96"
+            visible={true}
+          />
+        ) : (
+          <ImageGallery images={images} openModal={this.openModal} />
+        )}
         {this.state.selectedImage && (
           <Modal
             imageUrl={this.state.selectedImage}
